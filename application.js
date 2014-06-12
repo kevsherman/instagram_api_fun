6a0a627246947fb8aaf0f81a67743f35b172426f
kevsherman <kevsherman@gmail.com>
2014-06-12 01:32:20 +0000

    pulls JSON API data from search inputs and renders

--- application.js
+++ application.js
@@ -1,4 +1,20 @@
 $(document).ready(function(){
 
-  
-})
+  $('form#search').submit(function(event){
+    $('.city').remove();
+    var url = "http://autocomplete.wunderground.com/aq?query=";
+    var search = $('#searchfield').val();
+    var fullsearch = url + search + "&cb=?";
+
+    $.getJSON(fullsearch, function(data){
+
+      $cityArray = $(data.RESULTS);
+      CityViews.city($cityArray)
+    });
+
+    // var $maindiv = $('#main');
+
+    event.preventDefault();
+
+   });
+});
---
+++ city_views.js
@@ -0,0 +1,17 @@
+CityViews = {
+  city: function(cityArray){
+    $(cityArray).each(function(){
+      var city_name = "<h3 class='city'>"+this.name+"</h3>";
+      var href = CityViews.generateButton(this); 
+      $('#main').append(city_name);
+      $('#main').append(href);
+    });
+  },
+
+  generateButton: function(city){
+   
+    var uniqueId = city.zmw
+     console.log(uniqueId);
+    return "<button class='city' data-id='http://api.wunderground.com/api/e51ea3dd5554092e/forecast/q/zmw:"+uniqueId+".json'>visit page</a>";
+  }
+}
